60c1f34963edcfa0c11ecc419af0e7753aba0e20
Jonathan Brown <jonathan.brown@vertafore.com>
2019-01-09 14:56:40 -0700

    Update ACLs and Auth user creation script and remove old version

--- kafka/create-acls-and-auth.sh
+++ kafka/create-acls-and-auth.sh
@@ -1,13 +1,15 @@
+ZOOKEEPER=zk
+
 # Give permissions to ALICE
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=zk:2181 --add --allow-principal User:alice --operation All --topic test --resource-pattern-type PREFIXED
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:alice --operation All --topic test --resource-pattern-type PREFIXED
 
 # Give permissions to BOB
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=zk:2181 --add --allow-principal User:bob --operation Read --topic test
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=zk:2181 --add --allow-principal User:bob --operation Read --group bob-group
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:bob --operation Read --topic test
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:bob --operation Read --group bob-group
 
 # List existing ACLs:
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=zk:2181 --list
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --list
 
 
 # Create auth user:
-bin/kafka-configs.sh --zookeeper zk:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin]' --entity-type users --entity-name admin
+bin/kafka-configs.sh --zookeeper ${ZOOKEEPER}:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin]' --entity-type users --entity-name admin
