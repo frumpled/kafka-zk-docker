cb2e2a3bb63fb0fb9458d0af6f76ab754f1a1d07
Jonathan Brown <jonathan.brown@vertafore.com>
2019-03-01 17:33:52 -0700

    Refactor create-acls script and add sym link in Dockerfile

--- kafka/create-acls-and-auth.sh
+++ kafka/create-acls-and-auth.sh
@@ -1,17 +1,17 @@
-ZOOKEEPER=zk
+ZOOKEEPER=zk:2181
 
 # Give permissions to ALICE
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:alice --operation All --topic test --resource-pattern-type PREFIXED
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER} --add --allow-principal User:alice --operation All --topic test --resource-pattern-type PREFIXED
 
 # Give permissions to BOB
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:bob --operation Read --topic test
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --add --allow-principal User:bob --operation Read --group bob-group
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER} --add --allow-principal User:bob --operation Read --topic test
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER} --add --allow-principal User:bob --operation Read --group bob-group
 
 # List existing ACLs:
-bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER}:2181 --list
+bin/kafka-acls.sh --authorizer kafka.security.auth.SimpleAclAuthorizer --authorizer-properties zookeeper.connect=${ZOOKEEPER} --list
 
 
 # Create auth user:
-bin/kafka-configs.sh --zookeeper ${ZOOKEEPER}:2181 --alter --add-config 'SCRAM-SHA-512=[password=admin]' --entity-type users --entity-name admin
-bin/kafka-configs.sh --zookeeper ${ZOOKEEPER}:2181 --alter --add-config 'SCRAM-SHA-512=[password=alice]' --entity-type users --entity-name alice
-bin/kafka-configs.sh --zookeeper ${ZOOKEEPER}:2181 --alter --add-config 'SCRAM-SHA-512=[password=bob]' --entity-type users --entity-name bob
+bin/kafka-configs.sh --zookeeper ${ZOOKEEPER} --alter --add-config 'SCRAM-SHA-512=[password=admin]' --entity-type users --entity-name admin
+bin/kafka-configs.sh --zookeeper ${ZOOKEEPER} --alter --add-config 'SCRAM-SHA-512=[password=alice]' --entity-type users --entity-name alice
+bin/kafka-configs.sh --zookeeper ${ZOOKEEPER} --alter --add-config 'SCRAM-SHA-512=[password=bob]' --entity-type users --entity-name bob
